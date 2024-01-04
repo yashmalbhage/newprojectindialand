420dc13261e722682b9f9d96146ef27b1c590f71
Yashmalbhage01 <87193552+Yashmalbhage01@users.noreply.github.com>
2024-01-04 15:16:48 +0530

    first commit

---
+++ src/components/data/Data.js
@@ -0,0 +1,246 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    // path: "/about",
+  },
+  {
+    text: "Projects",
+    // path: "/services",
+  },
+  {
+    text: "blog",
+    // path: "/blog",
+  },
+  {
+    // text: "pricing",
+    // path: "/pricing",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+]
+export const featured = [
+  
+  {
+    cover: "../images/hero/h2.png",
+    name: "Area Development",
+    total: "155+ Property",
+  },
+  {
+    cover: "../images/hero/h3.png",
+    name: "IT parks",
+    total: "300 Property",
+  },
+  {
+    cover: "../images/hero/h4.png",
+    name: "Commercial spaces",
+    total: "80 Property",
+  },
+  {
+    cover: "../images/hero/h6.png",
+    name: "Under construction",
+    total: "80 Property",
+  },
+  {
+    cover: "../images/hero/h6.png",
+    name: "Godowns",
+    total: "80 Property",
+  },
+]
+export const list = [
+  {
+    id: 1,
+    cover: "../images/Chennai/1.jpg",
+    name: "INDIALAND INDUSTRIAL AND LOGISTIC PARK ORAGADAM",
+    location: "Chennai",
+    // category: "For Rent",
+    price: "Know more",
+    // type: "Apartment",
+  },
+  {
+    id: 2,
+    cover: "../images/Coimbatore/1.jpg",
+    name: "Indialand Tech park",
+    location: "Coimbatore",
+    // category: "For Sale",
+    price: "Know more",
+    // type: "Condos",
+  },
+  {
+    id: 3,
+    cover: "../images/Pune/1.jpg",
+    name: "IndiaLand Global industrial park ",
+    location: "Pune",
+    // category: "For Rent",
+    price: "Know more",
+    // type: "Offices",
+  },
+ 
+]
+export const awards = [
+  {
+    icon: <i class='fa-solid fa-trophy'></i>,
+    num: "32+	",
+    name: "MILLION SQFT OF TENANTED SPACE",
+  },
+  {
+    icon: <i class='fa-solid fa-briefcase'></i>,
+    num: "50+",
+    name: "MULTINATIONAL CLIENTS",
+  },
+  
+  {
+    icon: <i class='fa-solid fa-lightbulb'></i>,
+    num: "10+",
+    name: "II & INDUSTRAIL PARKS",
+  },
+]
+export const location = [
+  {
+    id: 1,
+    name: "Pune",
+    Villas: "",
+    Apartments: "",
+    Offices: "",
+    cover: "./images/location/city-1.png",
+  },
+  {
+    id: 2,
+    name: "Mumbai",
+    Villas: "",
+    Apartments: "",
+    Offices: "",
+    cover: "./images/location/city-2.png",
+  },
+  {
+    id: 3,
+    name: "Coimbatore",
+    Villas: "",
+    Apartments: "",
+    Offices: "",
+    cover: "./images/Coimbatore/1.jpg",
+  },
+ 
+]
+export const team = [
+  {
+    list: "ChairPerson",
+    cover: "../images/chairperson.png",
+    address: "India",
+    name: "Harish Fabiani",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "CEO",
+    cover: "../images/CEO.png",
+    address: "India",
+    name: "Salai Kumaran",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "COO",
+    cover: "../images/COO.png",
+    address: "Denever, USA",
+    name: "Rishi Choudhary",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  
+]
+export const price = [
+  {
+    plan: "Basic",
+    price: "29",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "120GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "5GB Cloud Storage",
+      },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Personal Help Support" },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Enterprise SLA" },
+    ],
+  },
+  {
+    best: "Best Value",
+    plan: "Standard",
+    price: "49",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "150GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "10GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        change: "color",
+        icon: <i class='fa-solid fa-x'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+  {
+    plan: "Platinum",
+    price: "79",
+    ptext: "2 user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "100% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "200GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "20GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+]
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+  },
+  {
+    title: "ALL SECTIONS",
+    text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+  },
+  {
+    title: "COMPANY",
+    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+  },
+]
